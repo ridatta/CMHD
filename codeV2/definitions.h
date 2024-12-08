7320a65c828c8b077e216d9365bf9c64ca6c4314
ridatta <78917991+ridatta@users.noreply.github.com>
2024-12-08 09:21:45 -0500

    Delete codeV2 directory

--- codeV2/definitions.h
+++
@@ -1,9 +0,0 @@
-// Definitions.h
-#include "input.h"
-
-double *S;
-double **rho, **u, **v, **p, **Bx, **By;
-double **internal_bound;
-double ***U, ***Up, ***Fx, ***Fxp, ***Fy, ***Fyp;
-double dx = 1.0/(double)nx; // Grid space
-
